15d34e42cdc5ad5c89a08e7662e6fab15bcc5a9e
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-02 10:35:37 +0300

    update logout controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,9 +9,11 @@ const {
   verifyEmail,
 } = require('../controllers/authController');
 
+const { authenticateUser } = require('../middleware/authentication');
+
 router.post('/register', register);
 router.post('/verify-email', verifyEmail);
 router.post('/login', login);
-router.get('/logout', logout);
+router.delete('/logout', authenticateUser, logout);
 
 module.exports = router;
